2a2d7cf2e0d60f91e19a41a19cf0738ff8f24611
Jacci <33001576+jacciz@users.noreply.github.com>
2022-06-06 17:14:47 -0500

    added bayesian opt.

--- read_data.R
+++ read_data.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(data.table)
 # options("scipen"=100, "digits"=4)
-path = "data/train/2020-05-28-US-MTV-2/GooglePixel4/"
+path = "data/train/2020-05-28-US-MTV-2/GooglePixel4XL/"
 gnss_df = read.csv(paste0(path, "device_gnss.csv")) |> setDT()
 i = read.csv(paste0(path, "device_imu.csv"))|> setDT()
 truth = read.csv(paste0(path, "ground_truth.csv"))|> setDT()
